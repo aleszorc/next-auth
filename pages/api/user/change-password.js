34203b27d23d8df6aad1492e214d62e023e63421
Lex-124 <ales.zorc1@gmail.com>
2021-05-30 09:27:55 +0200

    adding change password api

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from 'next-auth/client';
+import { hashPassword, verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -7,10 +9,48 @@ async function handler(req, res) {
 
   const session = await getSession({ req: req });
 
+  // The key part to protect the api from unauthorized access
   if (!session) {
     res.status(401).json({ message: 'Not authenticated!' });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection('users');
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found.' });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: 'Invalid password.' });
+    client.close();
+    res.status();
+    return;
+  }
+
+  const hashedPassword = await ashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: 'Password updated!' });
 }
 
 export default handler;
